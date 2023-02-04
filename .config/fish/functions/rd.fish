24a839170aea723645d8aa13a1b21083209fd7a4
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2023-02-05 03:11:11 +0500

    fix: minor improvement

--- .config/fish/functions/rd.fish
+++
@@ -1,4 +0,0 @@
-function rd
-    /Applications/RDM.app/Contents/MacOS/SetResX -d 0 --width 3840 --height 2160 --scale 2.0 --bits 32
-    /Applications/RDM.app/Contents/MacOS/SetResX -d 0 --width 2560 --height 1440 --scale 2.0 --bits 32
-end
