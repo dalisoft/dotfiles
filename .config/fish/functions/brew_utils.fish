1a31a714f80dc8c94fb24714795d038e5170c57f
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2025-11-23 13:40:59 +0500

    fix(fish): update_all function improve

--- .config/fish/functions/brew_utils.fish
+++ .config/fish/functions/brew_utils.fish
@@ -10,16 +10,36 @@ function brew_cleanup
     brew cleanup -s
 end
 
+function tool_cleanup
+    npm cache clean --force
+    pnpm store prune
+    yarn cache clean
+    bun pm cache rm --global
+
+    uv cache prune --force
+    uv cache clean --force
+end
+
 function brew_upgrade
     brew update --verbose
     brew upgrade --formula --verbose
     brew upgrade --cask --greedy --verbose
 end
 
-function update_all
-    brew_upgrade
-    brew_cleanup
+function tool_upgrade
     uv tool upgrade --all
-    bun x npm-check --global --update-all
-    update_repos
+    bun x npm-check --global --update-all --ignore '@sourcegraph/amp'
+end
+
+function update_all
+    brew_cleanup
+    tool_cleanup
+
+    brew_upgrade
+    tool_upgrade
+    # TODO: fix later
+    # update_repos
+
+    brew_cleanup
+    tool_cleanup
 end
